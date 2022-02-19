86df2c67d4721d47c3d224a513045d31e4ca0f45
kimgwhjg <gwkim@pusan.ac.kr>
2022-02-19 22:43:46 +0900

    초기설정

---
+++ prepare/front/pages/profile.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Head from "next/head";
+
+const profile = () => {
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <title>profile | NodeBird</title>
+        <div>profile</div>;
+      </Head>
+    </>
+  );
+};
+
+export default profile;
---
+++ prepare/front/pages/signup.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+function signup() {
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <title>signup | NodeBird</title>
+        <div>signup</div>;
+      </Head>
+    </>
+  );
+}
+
+export default signup;
